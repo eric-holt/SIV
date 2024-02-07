0873540baccff36da89fd9c75787b011d346d922
eric-holt <eric.stat.git@gmail.com>
2024-02-07 15:56:57 +0100

    added support for ggplot, data.frame (and the like), datatables, and multiple plots/tables/

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
-# Simple Plotly Viewer
-Render Plotly plots from RDS files via drag-and-drop. Instantly view and share interactive plots.
+# Simple Interactive Viewer
+Render multiple Plotly plots and DT tables from RDS files via drag-and-drop. Instantly view and share interactive plots and tables.
 
 ## Usage
 
-1. Save Plotly plot as RDS
+1. Save a supported R object (see below) as RDS
 2. Drag the RDS file to Viewer
-3. View plot
+3. View plot/table interactively
+4. Add (`+`) more files as you need 
 
-## Requirements
-- Plotly plot in RDS format
+## Supported object classes
+- `plotly`
+- `ggplot`
+- `data.frame` (and others like `data.table`)
+- (`datatables`)
 
-##
-RDS: R Data Structure
+## Notes
 
-Save your plot with `saveRDS()` in R.
+Use `saveRDS()` to save an object in R. 
+
+This app converts ggplot into Plotly using `ggplotly()` and data.frame-like object (like data.table) into DT using `datatable()`. 
+
+DT objects work locally but fails in the [shinyapps.io](https://a4d9lq-eric-holt.shinyapps.io/SimplePlotlyViewer/) instance. Just use the original table.
+
 
 Available at: 
 <https://a4d9lq-eric-holt.shinyapps.io/SimplePlotlyViewer/>
--- app.R
+++ app.R
@@ -1,34 +1,34 @@
 library(shiny)
 library(plotly)
+library(DT)
 source("css.R")
+source("display_module.R")
 
 ui = fixedPage(
   titlePanel("Simple Plotly Viewer"),
   tags$head(tags$style(HTML(css))),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file", "Drag and drop or choose a plotly .rds file", 
-                accept = c(".rds"),
-                multiple = FALSE,
-                buttonLabel = "Browse...",
-                placeholder = "No file selected"),
-      sliderInput("width", "Width", 
-                  min = 300, max = 1000, value = 600, step = 50),
-      sliderInput("height", "Height", 
-                  min = 300, max = 1000, value = 600, step = 50),
-      radioButtons("radio", "Theme", c("Light", "Dark"), "Dark", T,)
-    ),
-    mainPanel(
-      plotlyOutput("plot")
-    )
-  )
+  uiOutput("UI"),
+  actionButton("add", "", icon("plus"))
 )
 
 server = function(input, output) {
-  output$plot = renderPlotly({
-    req(input$file)
-    plot_data = readRDS(input$file$datapath)
-    plot_data |> plotly::layout(autosize = F, width = input$width, height = input$height)
+  num_module = reactiveVal(1)
+  
+  observe({
+    num_module(num_module() + 1)
+  }) |> bindEvent(input$add)
+  
+  output$UI = renderUI({
+    module_list = 1:num_module() |> lapply(function(i){
+      display_module_ui(i)
+    })
+    do.call(tagList, module_list)
+  })
+  
+  observe({
+    for(i in 1:num_module()){
+      display_module_server(i)
+    }
   })
 }
 
